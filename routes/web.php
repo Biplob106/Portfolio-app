5f97a562fba5d7dc85d4a286396ea480947e2a9e
Biplob106 <biplobsd106@gmail.com>
2023-12-12 23:22:57 +0600

    Admin Dashboard Added

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminHomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Admin Routes
+
+Route::get('/Admin/dashboard',[AdminHomeController::class,'index'])->name('admin.index');
+Route::get('/admin',[AdminHomeController::class,'view']);
